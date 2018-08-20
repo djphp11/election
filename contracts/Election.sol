85ca3c70875e938e4f33f33e67f438068a7d8f82
Philip Jung <djpj41@gmail.com>
2018-08-19 22:37:49 -0700

    1. Smoke Test

---
+++ contracts/Election.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.2;
+
+contract Election {
+	// Store candidate
+	// Read candidate
+	// Constructor
+	string public candidate;
+	function Election () public {
+		candidate = "Candidate 1";
+	}
+
+
+
+
+}
