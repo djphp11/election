7682e38b0f5357597825506ece6960c264a83d6d
Philip Jung <djpj41@gmail.com>
2018-08-20 18:45:54 -0700

    2. added candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,14 +1,33 @@
 pragma solidity ^0.4.2;
+import "./SafeMath.sol";
 
 contract Election {
-	// Store candidate
-	// Read candidate
-	// Constructor
-	string public candidate;
-	function Election () public {
-		candidate = "Candidate 1";
+	using SafeMath for uint256;
+	// Model a Candidate
+	
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
 	}
 
+	// Store Candidates
+	mapping(uint => Candidate) public candidates;
+	// Fetch Candidates
+
+	uint public candidatesCount;
+	// Store Candidates Count
+
+	function Election () public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");	
+	}
+
+
+	function addCandidate (string _name) private {
+		candidatesCount = candidatesCount.add(1);
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0); 
+	}
 
 
 
